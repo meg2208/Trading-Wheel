39c243b8ded30e0620bfc958fa921e93b2d43539
Nate Brennnand <natebrennand@gmail.com>
2013-03-23 21:46:08 -0400

    first working tables

---
+++ strategy.sql
@@ -0,0 +1,5 @@
+CREATE TABLE strategy(
+	strategy_id INTEGER,
+	strategy_name CHAR(20),
+	PRIMARY KEY (strategy_id)
+);
---
+++ userdata.sql
@@ -0,0 +1,5 @@
+CREATE TABLE userdata(
+	user_id CHAR(20),
+	password CHAR(20),
+	PRIMARY KEY (user_id)
+);
