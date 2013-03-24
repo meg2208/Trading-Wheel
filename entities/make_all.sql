e026ff70c3092ed028e13453cc60025783ee7451
Nate Brennnand <natebrennand@gmail.com>
2013-03-24 02:30:04 -0400

    added make_all sql files for both entities and relations

---
+++ entities/make_all.sql
@@ -0,0 +1,40 @@
+CREATE TABLE indicator(
+	field_data CHAR(10),
+	start_time DATE,
+	end_time DATE,
+	security CHAR(6),
+	indicator_id INTEGER,
+	PRIMARY KEY (indicator_id)
+);
+CREATE TABLE portfolio_statistics (
+	version INTEGER,
+	sharpe_ratio REAL,
+	returns REAL,
+	user_id CHAR(20),
+	PRIMARY KEY(user_id, version),
+	FOREIGN KEY (user_id) REFERENCES user_data
+);
+CREATE TABLE query_data (
+	time DATE,
+	security CHAR(6),
+	field_data char(10),
+	PRIMARY KEY (security, field_data)
+);
+CREATE TABLE strategy(
+	strategy_id INTEGER,
+	strategy_name CHAR(20),
+	PRIMARY KEY (strategy_id)
+);
+CREATE TABLE trade (
+	trade_id INTEGER,
+	buy_sell CHAR(1),
+	security CHAR(6),
+	time DATE,
+	portfolio_value REAL,
+	PRIMARY KEY (trade_id)
+);
+CREATE TABLE user_data(
+	user_id CHAR(20),
+	password CHAR(20),
+	PRIMARY KEY (user_id)
+);
---
+++ relations/make_all.sql
@@ -0,0 +1,57 @@
+CREATE TABLE action (
+	strategy_id INTEGER,
+	trade_id INTEGER,
+	PRIMARY KEY (strategy_id, trade_id),
+	FOREIGN KEY (strategy_id) REFERENCES strategy,
+	FOREIGN KEY (trade_id) REFERENCES trade
+);
+CREATE TABLE create_portfolio (
+	strategy_id INTEGER,
+	version INTEGER,
+	user_id CHAR(20),
+	PRIMARY KEY (strategy_id, user_id, version),
+	FOREIGN KEY (strategy_id) REFERENCES strategy,
+	FOREIGN KEY (user_id, version) REFERENCES portfolio_statistics
+);
+CREATE TABLE create_strategy (
+	user_id CHAR(20),
+	strategy_id INTEGER,
+	PRIMARY KEY (user_id, strategy_id ),
+	FOREIGN KEY (user_id) REFERENCES user_data,
+	FOREIGN KEY (strategy_id) REFERENCES strategy
+);
+CREATE TABLE criteria (
+	strategy_id INTEGER,
+	indicator_id INTEGER,
+	PRIMARY KEY (strategy_id, indicator_id),
+	FOREIGN KEY (strategy_id) REFERENCES strategy,
+	FOREIGN KEY (indicator_id) REFERENCES indicator
+);
+CREATE TABLE raw_data_parsing (
+	strategy_id INTEGER,
+	security CHAR(6),
+	field_data char(10),
+	PRIMARY KEY (strategy_id, security),
+	FOREIGN KEY (strategy_id) REFERENCES strategy,
+	FOREIGN KEY (security, field_data) REFERENCES query_data
+);
+CREATE TABLE references(
+	action_security CHAR(6),
+	-- BOOLEAN: Must be 'Y' or 'N'
+	buy_sell CHAR(1),
+	operator char(10),
+	share_amount INTEGER,
+	allocation REAL,
+	cash_value REAL,
+	indicator_user INTEGER,
+	-- The user uses the reference
+	indicator_ref INTEGER,
+	PRIMARY KEY (indicator_user, indicator_ref),
+	FOREIGN KEY (indicator_user) REFERENCES indicator,
+	FOREIGN KEY (indicator_ref) REFERENCES indicator
+);
+CREATE TABLE user_data(
+	user_id CHAR(20),
+	password CHAR(20),
+	PRIMARY KEY (user_id)
+);
