52711d2bf0a2eda101300fe9b729cc2020b73728
Nate Brennnand <natebrennand@gmail.com>
2013-03-23 23:09:43 -0400

    new queries to improve workflow

---
+++ all.sql
@@ -0,0 +1,4 @@
+-- Displays all tables within the database
+-- includes ours, and all others
+SELECT owner, table_name
+FROM all_tables;
---
+++ mine.sql
@@ -0,0 +1,4 @@
+-- Displays all of my tables
+SELECT owner, table_name
+FROM all_tables
+WHERE owner = 'NSB2142';
