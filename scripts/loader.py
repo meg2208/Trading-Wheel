e72e79b7d3527d3bbada929a6dea330028195657
Nate Brennnand <natebrennand@gmail.com>
2013-03-26 03:11:47 -0400

    fixed small formatting error

--- scripts/loader.py
+++ scripts/loader.py
@@ -36,7 +36,7 @@ def insert_data(table,data,cursor,db):
 
 if __name__ == '__main__':
 	if len(argv) != 3:
-		print '\tProper Usage:\npython loader.py <table_name> <csv_file_name>' 
+		print 'Proper Usage:\n\tpython loader.py <table_name> <csv_file_name>' 
 		exit(1)
 	# Catching argv variables
 	table = str(argv[1])
