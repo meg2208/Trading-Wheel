3ea3ad65c017fb319981c08c155ed569e6ee0843
Matt Garbis <mgarbis@gmail.com>
2013-03-26 16:00:25 -0400

    added strategy_id as foreign/primary key to indicator entity
    (had to make this change so both right and left indicator have same id)
    fixed criteria.csv and created indicator.csv

--- entities/sql/indicator.sql
+++ entities/sql/indicator.sql
@@ -1,10 +1,12 @@
 CREATE TABLE indicator(
+	strategy_id INTEGER,
 	start_time DATE,
 	end_time DATE,
 	security CHAR(6),
 	indicator_id INTEGER,
 	-- Can only be 'Y' or 'N'
 	preceding_operator CHAR(1),
-	PRIMARY KEY (indicator_id, preceding_operator)
+	PRIMARY KEY (strategy_id, indicator_id)
+	FOREIGN KEY (strategy_id) references strategy
 );
 
