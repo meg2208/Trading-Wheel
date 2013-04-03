18853a8c1037325667592540f20ba96c48162ca8
meg2208 <mgarbis@gmail.com>
2013-04-03 17:23:25 -0300

    fixed syntax errors

--- relations/sql/portfolio_contents.sql
+++ relations/sql/portfolio_contents.sql
@@ -1,6 +1,7 @@
 CREATE TABLE portfolio_contents (
-  state_id INTEGER;
-	portfolio_id INTEGER;
+	state_id INTEGER,
+	portfolio_id INTEGER,
 	PRIMARY KEY (state_id, portfolio_id),
 	FOREIGN KEY (state_id) REFERENCES security_state,
-	FOREIGN KEY (portfolio_id) REFERENCES aggregate_portfolio;
+	FOREIGN KEY (portfolio_id) REFERENCES aggregate_portfolio,
+);
