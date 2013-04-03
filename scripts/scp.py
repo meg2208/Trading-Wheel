b5460099e8042d352d9f35a1343184c745f9c8c9
Nate Brennnand <natebrennand@gmail.com>
2013-04-03 02:28:27 -0400

    Small adjustment

--- scripts/scp.py
+++ scripts/scp.py
@@ -1,6 +1,6 @@
 
 from os import system
 
-scp_command = 'scp nsb2142@clic-lab.cs.columbia.edu:/home/nsb2142/expdat.dmp /Users/nate/GitHub/Trading-Wheel/expdat.dmp'
+scp_command = 'scp nsb2142@clic-lab.cs.columbia.edu:/home/nsb2142/cs4111/expdat.dmp /Users/nate/GitHub/Trading-Wheel/expdat.dmp'
 
 system( scp_command )
