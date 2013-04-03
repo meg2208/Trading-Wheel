25515bda1dfe82acffcc80f5a4010a4df8928dfc
meg2208 <mgarbis@gmail.com>
2013-04-03 17:09:15 -0300

    Create portfolio_contents.sql

---
+++ relations/sql/portfolio_contents.sql
@@ -0,0 +1,6 @@
+CREATE TABLE portfolio_contents (
+  state_id INTEGER;
+	portfolio_id INTEGER;
+	PRIMARY KEY (state_id, portfolio_id),
+	FOREIGN KEY (state_id) REFERENCES security_state,
+	FOREIGN KEY (portfolio_id) REFERENCES aggregate_portfolio;
