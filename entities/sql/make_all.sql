3d44ffff54f550e0b706dc2df02ec77406feb46c
meg2208 <mgarbis@gmail.com>
2013-04-03 16:13:42 -0300

    Updated indicator mva attribute names

--- entities/sql/make_all.sql
+++ entities/sql/make_all.sql
@@ -51,8 +51,8 @@ CREATE TABLE indicator(
 	start_time DATE,	-- DD-MMM-YYYY
 	end_time DATE,		-- DD-MMM-YYYY
 	security CHAR(6),
-	mva_50_day CHAR(1),
-	mva_200_day CHAR(1),
+	mva_10_day CHAR(1),
+	mva_25_day CHAR(1),
 	PRIMARY KEY (indicator_id)
 );
 CREATE TABLE current_portfolio(
