46a9c5286a4043c71dbd3df80097afb51e2e5966
Matt Garbis <mgarbis@gmail.com>
2013-04-11 14:58:13 -0400

    integrated backtest prep sql to controller.py

--- queries/mva.sql
+++ queries/mva.sql
@@ -8,20 +8,3 @@ USING (SELECT DISTINCT q.time, AVG(q.adj_close) OVER(ORDER BY q.time ROWS BETWEE
 		WHERE q.security = 'AAPL') B
 	ON (A.time = B.time AND A.security = 'AAPL')
 	WHEN MATCHED THEN UPDATE SET A.MVA_10_day = B.mv10, A.MVA_25_DAY = B.mv25;
-
-
-
--- deletes row	
--- UPDATE query_data SET MVA_10_day = NULL, MVA_25_DAY = NULL where security = 'AAPL';
-
--- queries data
--- SELECT * from query_data where security = 'AAPL' order by time;
-
-
--- working query for mva
---SELECT q1.security, q1.time , q1.adj_close, q1.MVA_10_day, q1.MVA_25_DAY, AVG(q1.adj_close) 
---			OVER(ORDER BY q1.security ROWS BETWEEN 24 PRECEDING 
---					AND CURRENT ROW) AS mv25				
---			FROM query_data q1 
---			WHERE q1.security = 'AAPL'
---			ORDER BY q1.time;
--- queries/pop_temp_s_x_under.sql
+++ queries/pop_temp_s_x_under.sql
@@ -1,16 +1,29 @@
-INSERT INTO TRADE (trade_id, security, action, share_amount, allocation, price, time)
-	Select seq_tid.nextval, security, buy_sell, share_amount, allocation, adj_close, time
-	  FROM ( 
-			SELECT q.security, ir.operator, ir.buy_sell, ir.share_amount, ir.allocation, q.adj_close, q.time, SUM(q.MVA_10_DAY) OVER(ORDER BY q.time ROWS BETWEEN 1 PRECEDING 
-					AND CURRENT ROW) - q.MVA_10_DAY AS yestmva1, q.MVA_10_DAY AS mva1,
-				SUM(q.MVA_25_DAY) OVER(ORDER BY q.time ROWS BETWEEN 1 PRECEDING 
-					AND CURRENT ROW) - q.MVA_25_DAY AS yestmva2, q.MVA_25_DAY AS mva2
-			FROM indicator L_ind, indicator R_ind, query_data q, indicator_reference ir, raw_data_parsing rd
-			WHERE q.security = ir.action_security 
+INSERT ALL
+	INTO TRADE (trade_id, security, action, share_amount, allocation, price, time)
+		VALUES(seq_tid.nextval, security, 'S', share_amount, allocation, adj_close, time)
+	INTO MAKES_TRADE (trade_id, portfolio_id)
+		VALUES(seq_tid.currval, portfolio_id)
+SELECT portfolio_id, a.security, a.buy_sell, a.share_amount, a.allocation, a.adj_close, a.time
+	FROM ( 
+		SELECT rd.strategy_id, q.security, ir.operator, ir.buy_sell, ir.share_amount, ir.allocation, 
+				q.adj_close, q.time, 
+				SUM(q.MVA_10_DAY) OVER(ORDER BY q.time ROWS BETWEEN 1 PRECEDING AND CURRENT ROW) - q.MVA_10_DAY AS yestmva1, 
+			   	q.MVA_10_DAY AS mva1,
+				SUM(q.MVA_25_DAY) OVER(ORDER BY q.time ROWS BETWEEN 1 PRECEDING AND CURRENT ROW) - q.MVA_25_DAY AS yestmva2, 
+				q.MVA_25_DAY AS mva2
+		FROM indicator L_ind, indicator R_ind, query_data q, indicator_reference ir, raw_data_parsing rd
+		WHERE q.security = ir.action_security 
 			AND L_ind.MVA_10_DAY = 'F' 
 			AND R_ind.MVA_25_DAY = 'T' 
 			AND ir.buy_sell = 'S' 
 			AND rd.time = q.time 
 			AND ir.operator = 'x_under' 
-			AND q.security = ir.action_security)
-		WHERE mva1 < mva2 AND yestmva1 > yestmva2;
+			AND q.security = ir.action_security ) a,
+  	  (SELECT ag.portfolio_id, ag.time, dtd.strategy_id
+  			FROM aggregate_portfolio ag, day_to_day dtd
+  			WHERE ag.time = time
+  				AND ag.portfolio_id = dtd.portfolio_id
+  				AND dtd.strategy_id = 1000) b
+	WHERE a.mva1 < a.mva2 
+	  AND a.yestmva1 > a.yestmva2
+	  AND b.time = a.time;
