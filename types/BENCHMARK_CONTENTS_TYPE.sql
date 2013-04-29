4bf367c269797d454e082df3d9adfd90a84fe2a1
Matt Garbis <mgarbis@gmail.com>
2013-04-28 22:36:20 -0400

    had too many "END" statements

--- types/BENCHMARK_CONTENTS_TYPE.sql
+++ types/BENCHMARK_CONTENTS_TYPE.sql
@@ -19,17 +19,16 @@ CREATE OR REPLACE TYPE BODY BENCHMARK_CONTENTS_TYPE AS
 								strategy_id NUMBER,
 								security VARCHAR2,
 								allocation NUMBER)
-		RETURN SELF AS RESULT
-		AS
-		BEGIN
-			--sets start_date attribute
-				SELECT 	MIN(time) 
-				INTO SELF.start_date
-				FROM	raw_data_parsing
-				WHERE	strategy_id = SELF.strategy_id;
-			populate_shares(); -- sets share amount for this security
-			RETURN;
-		END;
+	RETURN SELF AS RESULT
+	AS
+	BEGIN
+		--sets start_date attribute
+			SELECT 	MIN(time) 
+			INTO SELF.start_date
+			FROM	raw_data_parsing
+			WHERE	strategy_id = SELF.strategy_id;
+		populate_shares(); -- sets share amount for this security
+		RETURN;
 	END;
 	-- fills in the correct amount of shares and start_value
 	MEMBER PROCEDURE populate_shares IS	
@@ -44,15 +43,14 @@ CREATE OR REPLACE TYPE BODY BENCHMARK_CONTENTS_TYPE AS
 				WHERE	s.strategy_id = SELF.strategy_id);
 		price price_curs%ROWTYPE;
 		port_val start_val_curs%ROWTYPE;
-		BEGIN 
-			OPEN price_curs;
-			OPEN p_val_curs;
-			FETCH price_curs INTO price;
-			FETCH p_val_curs INTO port_val;
-			SELF.shares := ROUND((port_val*SELF.allocation)/price);
-			SELF.start_value := SELF.shares*price;
-		END;
-	END;
+	BEGIN 
+		OPEN price_curs;
+		OPEN p_val_curs;
+		FETCH price_curs INTO price;
+		FETCH p_val_curs INTO port_val;
+		SELF.shares := ROUND((port_val*SELF.allocation)/price);
+		SELF.start_value := SELF.shares*price;
+	END populate_shares;
 	-- returns total value of this security in benchmark
 	MEMBER PROCEDURE get_value (given_time DATE) IS	
 		CURSOR price_curs(adj_close NUMBER) IS  
@@ -61,11 +59,10 @@ CREATE OR REPLACE TYPE BODY BENCHMARK_CONTENTS_TYPE AS
 				WHERE	q.security = SELF.security
 					AND q.time = given_time);
 		price price_curs%ROWTYPE;
-		BEGIN 
-			OPEN price_curs;
-			FETCH price_curs INTO price;
-			RETURN price*SELF.shares;
-		END;
-	END;
+	BEGIN 
+		OPEN price_curs;
+		FETCH price_curs INTO price;
+		RETURN price*SELF.shares;
+	END get_value;
 END;
 /
