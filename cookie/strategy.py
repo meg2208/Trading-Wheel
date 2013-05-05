66fb638d05712519f106f5f9dfae6feabe37bb25
Nate Brennnand <natebrennand@gmail.com>
2013-05-05 01:02:54 -0400

    added str() method to strategy
    also changed how triggers are instantiated

--- cookie/strategy.py
+++ cookie/strategy.py
@@ -5,5 +5,12 @@ class Strategy:
     def __init__(self, name, allocation, triggers=None):
         self.name = name
         self.allocation = allocation
-        self.triggers = triggers
+        if triggers:
+            self.triggers = triggers
+        else:
+            self.triggers = []
 
+    def __str__(self):
+        to_string = 'strategy: {}\nstarted w/: {}\ntriggers:{}\n'.format(
+            self.name, self.allocation, self.triggers)
+        return to_string
