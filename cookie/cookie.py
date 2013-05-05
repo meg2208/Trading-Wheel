74f3d17850a8bb3fdb952604001a6d2f6c192226
Nate Brennnand <natebrennand@gmail.com>
2013-05-05 00:40:51 -0400

    initial commit

---
+++ cookie/cookie.py
@@ -0,0 +1,7 @@
+
+
+class Cookie:
+
+    def __init__(self ):
+        pass
+
---
+++ cookie/strategy.py
@@ -0,0 +1,9 @@
+
+
+class Strategy:
+
+    def __init__(self, name, allocation, triggers=None):
+        self.name = name
+        self.allocation = allocation
+        self.triggers = triggers
+
